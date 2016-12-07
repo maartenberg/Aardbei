b62d35ed654866c6491145e1bcd058bf80a829a7
Maarten van den Berg <maartenberg1@gmail.com>
2016-12-07 21:55:12 +0100

    Add Static Pages controller for public pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def sign_in
+  end
+
+  def sign_up
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/sign_in'
+
+  get 'static_pages/sign_up'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
