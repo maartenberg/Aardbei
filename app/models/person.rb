2ee39da5ad0a1098b267cb939e894fd1db7fb3be
Maarten van den Berg <maartenberg1@gmail.com>
2016-12-08 10:45:52 +0100

    Add Person validation

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,20 @@
 class Person < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :is_admin, presence: true
+
+  validate :birth_date_cannot_be_in_future
+
+  before_validation :not_admin_if_nil
+
+  def birth_date_cannot_be_in_future
+    if self.birth_date > Date.today
+      errors.add(:birth_date, "can't be in the future.")
+    end
+  end
+
+  def not_admin_if_nil
+    self.is_admin ||= false
+  end
 end
