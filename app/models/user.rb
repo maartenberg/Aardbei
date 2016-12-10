7680dfb0813ff94d15237c55c987acf9e862f752
Maarten van den Berg <maartenberg1@gmail.com>
2016-12-10 20:26:25 +0100

    Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
   belongs_to :person
+
+  validates :person, presence: true
+  validates :email, uniqueness: true
+
+  before_validation :email_same_as_person
+
+  private
+  def email_same_as_person
+    if self.person and self.email != self.person.email
+      errors.add(:email, "must be the same as associated person's email")
+    end
+  end
 end
