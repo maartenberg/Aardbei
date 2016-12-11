5b3df5b45dfc3474218fcfe3c1a554e82d6c5b91
Maarten van den Berg <maartenberg1@gmail.com>
2016-12-11 12:56:41 +0100

    Update User email if Person changes email

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,6 @@
 class Person < ApplicationRecord
+  has_one :user
+
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
@@ -7,7 +9,9 @@ class Person < ApplicationRecord
   validate :birth_date_cannot_be_in_future
 
   before_validation :not_admin_if_nil
+  before_save :update_user_email, if: :email_changed?
 
+  private
   def birth_date_cannot_be_in_future
     if self.birth_date > Date.today
       errors.add(:birth_date, "can't be in the future.")
@@ -16,5 +20,9 @@ class Person < ApplicationRecord
 
   def not_admin_if_nil
     self.is_admin ||= false
+  end
+
+  def update_user_email
+    self.user.update!(email: self.email)
   end
 end
