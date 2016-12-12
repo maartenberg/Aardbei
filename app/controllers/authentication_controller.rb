08f548241fc4082e8fb9c88fae85042981fdc71d
Maarten van den Berg <maartenberg1@gmail.com>
2016-12-12 22:01:12 +0100

    Use form_for, stub login logic

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -4,7 +4,19 @@ class AuthenticationController < ApplicationController
   end
 
   def login
-    flash[:danger] = "Not yet implemented."
+    if params[:session][:email].blank? || params[:session][:password].blank?
+      flash[:warning] = "You forgot to add value"
+    else
+      u = User.find_by(email: params[:session][:email])
+
+      if u && u.authenticate(params[:session][:password])
+        # TODO Login logic here
+        flash[:success] = "Hello, #{u.person.full_name}!"
+      else
+        flash[:danger] = "Invalid username/password combination!"
+      end
+    end
+
     redirect_to action: 'login_form'
   end
 
