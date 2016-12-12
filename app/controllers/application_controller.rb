c9bf6acdf7972778a24c6099d91b704d6e2045db
Maarten van den Berg <maartenberg1@gmail.com>
2016-12-12 20:47:46 +0100

    Reacue from csrf with message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from ActionController::InvalidAuthenticityToken, with: :invalid_auth_token
+
+  private
+  def invalid_auth_token
+    render text: "You submitted an invalid request! If you got here after clicking a link, it's possible that someone is doing something nasty!", status: 400
+  end
 end
