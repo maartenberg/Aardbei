3cb88be0fce2399f87f09372ffb002ec69ab6656
Maarten van den Berg <maartenberg1@gmail.com>
2016-12-12 20:17:10 +0100

    Alerts, fix routing

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,13 +1,28 @@
 class AuthenticationController < ApplicationController
+  def login_form
+    render layout: 'void'
+  end
+
   def login
+    flash[:danger] = "Not yet implemented."
+    redirect_to action: 'login_form'
+  end
+
+  def create_password_form
     render layout: 'void'
   end
 
   def create_password
+    flash[:danger] = "Not yet implemented."
+    redirect_to action: 'login'
+  end
+
+  def forgotten_password_form
     render layout: 'void'
   end
 
   def forgotten_password
-    render layout: 'void'
+    flash[:danger] = "Not yet implemented."
+    redirect_to action: 'login'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'login', to: 'authentication#login'
+  get  'login', to: 'authentication#login_form'
+  post 'login', to: 'authentication#login'
 
-  get 'authentication/create_password'
+  get  'register', to: 'authentication#create_password_form'
+  post 'register', to: 'authentication#create_password'
 
-  get 'authentication/forgotten_password'
+  get  'forgot', to: 'authentication#forgotten_password_form'
+  post 'forgot', to: 'authentication#forgotten_password'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
