d0a51b00f875ac61f68435682e22e643b6e094ec
Maarten van den Berg <maartenberg1@gmail.com>
2016-12-22 17:28:03 +0100

    Add activity validation

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,4 +3,23 @@ class Activity < ApplicationRecord
 
   has_many :participants
   has_many :people, through: :participants
+
+  validates :public_name, presence: true
+  validates :start, presence: true
+  validate  :deadline_before_start
+
+  def organizers
+    self.organizers.includes(:person).where(is_organizer: true)
+  end
+
+  def is_organizer(person)
+    Participant.exists?(person_id: person.id, activity_id: self.id, is_organizer: true)
+  end
+
+  private
+  def deadline_before_start
+    if self.deadline && self.deadline > self.start
+      errors.add(:deadline, 'must be before start')
+    end
+  end
 end
