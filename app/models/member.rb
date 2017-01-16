7e7585eef3c8b000c8beb35298f1cef389dce978
Maarten van den Berg <maartenberg1@gmail.com>
2017-01-17 00:09:10 +0100

    Delete future participants on group leave

--- app/models/member.rb
+++ app/models/member.rb
@@ -8,9 +8,27 @@ class Member < ApplicationRecord
   belongs_to :person
   belongs_to :group
 
+  before_destroy :delete_future_participants!
+
   validates :person_id,
     uniqueness: {
       scope: :group_id,
       message: "is already a member of this group"
     }
+
+  # Delete all Participants of this Member for Activities in the future.
+  # Intended to be called before the member is deleted.
+  def delete_future_participants!
+    activities = self.group.activities
+      .where('start > ?', DateTime.now)
+
+    participants = Participant.where(
+      person_id: self.person.id,
+      activity: activities
+    )
+
+    participants.each do |p|
+      p.destroy!
+    end
+  end
 end
