d714496afd626495d131cf4c83970a68c8219b7b
Maarten van den Berg <maartenberg1@gmail.com>
2017-09-06 16:15:07 +0200

    Fix is_organizer=nil case

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -40,7 +40,7 @@ class ActivitiesController < ApplicationController
 
   # GET /activities/1/edit
   def edit
-    @non_organizers = @activity.participants.where.not(is_organizer: true)
+    @non_organizers = @activity.participants.where(is_organizer: [true, nil])
     @organizers = @activity.organizers
 
     @non_organizers_options = @non_organizers.map{|p| [p.person.full_name, p.id] }
