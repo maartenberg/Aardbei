7035281421569600266e4cf864bd0fe909f16ee3
Maarten van den Berg <maartenberg1@gmail.com>
2017-09-12 21:37:17 +0200

    Make mail configurable between SMTP and Mailgun

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,13 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.delivery_method = ENV['MAIL_METHOD']
+  config.action_mailer.smtp_settings = {
+    address: ENV['SMTP_SERVER'],
+    user_name: ENV['SMTP_USER'],
+    password: ENV['SMTP_PASS'],
+    authentication: :plain
+  }
   config.action_mailer.mailgun_settings = {
         api_key: ENV['MAILGUN_API_KEY'],
         domain: ENV['MAILGUN_DOMAIN']
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.delivery_method = ENV['MAIL_METHOD']
+  config.action_mailer.smtp_settings = {
+    address: ENV['SMTP_SERVER'],
+    user_name: ENV['SMTP_USER'],
+    password: ENV['SMTP_PASS'],
+    authentication: :plain
+  }
   config.action_mailer.mailgun_settings = {
         api_key: ENV['MAILGUN_API_KEY'],
         domain: ENV['MAILGUN_DOMAIN']
