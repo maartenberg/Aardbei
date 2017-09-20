17abdcd52f40aac89583ad2b7912beb4a50f2652
Maarten van den Berg <maartenberg1@gmail.com>
2017-09-20 13:23:01 +0200

    Membership requires login

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,5 +1,6 @@
 module ActivitiesHelper
   def require_organizer!
+    require_login!
     if !@activity.may_change?(current_person)
       flash_message(:danger, I18n.t('authentication.organizer_required'))
       redirect_to group_activity_path(@group, @activity)
